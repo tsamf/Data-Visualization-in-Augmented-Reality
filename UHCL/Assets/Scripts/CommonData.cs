d03256c706f2a4ac74242313d29486aad50b211e
Travis Samford <samfordt@gmail.com>
2018-02-21 19:33:49 -0600

    BackBone
    Initial Backbone Check in

---
+++ UHCL/Assets/Scripts/CommonData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommonData : MonoBehaviour {
+
+    private bool editMode = true;
+
+	public bool EditMode { get { return editMode; } set { editMode = value; } }
+
+}
---
+++ UHCL/Assets/Scripts/Keywords.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using System;
+
+public class Keywords : MonoBehaviour,ISpeechHandler {
+
+    //Need Instance of buisnees layer 
+    public bool IsGlobalListener = true;
+
+    public TextMesh testText;
+
+    public GameObject commonData;
+    private CommonData data;
+
+    private void OnEnable()
+    {
+        InputManager.Instance.AddGlobalListener(gameObject);
+    }
+
+    void Awake()
+    {
+        data = commonData.GetComponent<CommonData>();
+    }
+
+    public void OnSpeechKeywordRecognized(SpeechEventData eventData)
+    {
+        switch (eventData.RecognizedText.ToLower())
+        {
+            case "start edit mode":
+                {
+                    StartEditMode();
+                    break;
+                }
+            case "stop edit mode":
+                {
+                    StopEditMode();
+                    break;
+                }
+            default:
+                break;
+        }
+    }
+
+    private void StopEditMode()
+    {
+        testText.text = "Stop edit mode.";
+        data.EditMode = false;
+    }
+
+    private void StartEditMode()
+    {
+        testText.text = "Start edit mode.";
+        data.EditMode = true;
+    }
+}
