9dbb3d9153606933fbdfc44b8fc94e1b4ae8df05
Travis Samford <samfordt@gmail.com>
2018-02-27 11:52:53 -0600

    Added a basic CSV Parser
    Needs a little work but is usable for thursdays test.

---
+++ UHCL/Assets/Scripts/DB_CSVFileParser.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class DB_CSVFileParser : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        string filePathRead = Application.streamingAssetsPath + "/Disabling Alarm Procedure.csv";
+        string filePathWrite = Application.streamingAssetsPath + "/Disabling Alarm Procedure Write Test.csv";
+
+        List<string[]> fileContents = readfile(filePathRead, true);
+        WriteFile(filePathWrite, fileContents);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    List<string[]> readfile(string path, bool removeHeader)
+    {
+
+        StreamReader reader = new StreamReader(path);
+        List<string[]> rows = new List<string[]>();
+
+        //If file has header and needs to be removed remove it.
+        if (removeHeader && !reader.EndOfStream)
+        {
+            reader.ReadLine();
+        }
+
+        while (!reader.EndOfStream)
+        {
+            string[] row = reader.ReadLine().Split(',');
+            rows.Add(row);
+        }
+
+        reader.Close();
+
+        return rows;
+    }
+
+    void WriteFile(string path, List<string[]> rows)
+    {
+        StreamWriter writer = new StreamWriter(path);
+
+        foreach (string[] r in rows)
+        {
+            string row = "";
+            for (int i = 0; i < r.Length; i++)
+            {
+                //Last value dont add comma at the end
+                if (i == r.Length - 1)
+                {
+                    row += (r[i]);
+                }
+                else
+                {
+                    row += (r[i] + ",");
+                }
+
+            }
+
+            writer.WriteLine(row);
+        }
+
+        writer.Close();
+    }
+}
