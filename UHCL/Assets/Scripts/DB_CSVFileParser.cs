04b0236185d1f8551aa6552c78b41d6a80279b44
Travis Samford <samfordt@gmail.com>
2018-03-05 12:10:15 -0600

    Commented out Database code
    The current way we are reading files causes build errors. The code has been commented out for now.

--- UHCL/Assets/Scripts/DB_CSVFileParser.cs
+++ UHCL/Assets/Scripts/DB_CSVFileParser.cs
@@ -5,53 +5,53 @@ using System.IO;
 
 public static  class DB_CSVFileParser {
     
-    public static List<string[]> readfile(string path, bool removeHeader)
-    {
+    //public static List<string[]> readfile(string path, bool removeHeader)
+    //{
 
-        StreamReader reader = new StreamReader(path);
-        List<string[]> rows = new List<string[]>();
+    //    StreamReader reader = new StreamReader(path);
+    //    List<string[]> rows = new List<string[]>();
 
-        //If file has header and needs to be removed remove it.
-        if (removeHeader && !reader.EndOfStream)
-        {
-            reader.ReadLine();
-        }
+    //    //If file has header and needs to be removed remove it.
+    //    if (removeHeader && !reader.EndOfStream)
+    //    {
+    //        reader.ReadLine();
+    //    }
 
-        while (!reader.EndOfStream)
-        {
-            string[] row = reader.ReadLine().Split(',');
-            rows.Add(row);
-        }
+    //    while (!reader.EndOfStream)
+    //    {
+    //        string[] row = reader.ReadLine().Split(',');
+    //        rows.Add(row);
+    //    }
 
-        reader.Close();
+    //    reader.Close();
 
-        return rows;
-    }
+    //    return rows;
+    //}
 
-    public static void WriteFile(string path, List<string[]> rows)
-    {
-        StreamWriter writer = new StreamWriter(path);
+    //public static void WriteFile(string path, List<string[]> rows)
+    //{
+    //    StreamWriter writer = new StreamWriter(path);
 
-        foreach (string[] r in rows)
-        {
-            string row = "";
-            for (int i = 0; i < r.Length; i++)
-            {
-                //Last value dont add comma at the end
-                if (i == r.Length - 1)
-                {
-                    row += (r[i]);
-                }
-                else
-                {
-                    row += (r[i] + ",");
-                }
+    //    foreach (string[] r in rows)
+    //    {
+    //        string row = "";
+    //        for (int i = 0; i < r.Length; i++)
+    //        {
+    //            //Last value dont add comma at the end
+    //            if (i == r.Length - 1)
+    //            {
+    //                row += (r[i]);
+    //            }
+    //            else
+    //            {
+    //                row += (r[i] + ",");
+    //            }
 
-            }
+    //        }
 
-            writer.WriteLine(row);
-        }
+    //        writer.WriteLine(row);
+    //    }
 
-        writer.Close();
-    }
+    //    writer.Close();
+    //}
 }
