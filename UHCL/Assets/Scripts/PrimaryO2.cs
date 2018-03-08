d94035d3cf1f5e49663e08f2ac3f2d409f682a1d
KadamA19 <36745180+KadamA19@users.noreply.github.com>
2018-03-08 15:02:09 -0600

    Initial Pie design
    Shrink Primary O2 pie using values from console

---
+++ UHCL/Assets/Scripts/PrimaryO2.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class PrimaryO2 : MonoBehaviour {
+
+    // Use this for initialization
+    public float radius = 1.0f;
+    public Image obj;
+    public float currentHealth;
+    public GameObject busLayer;
+    private BL_Main pressurelog;
+
+    void Start () {
+        currentHealth = (Console.Read());
+        //Assign max value to cuurent health status for initialization
+        pressurelog = busLayer.GetComponent<BL_Main>();
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        radius = currentHealth;
+
+
+        if (currentHealth >= 0.5)
+            Debug.Log("HH");
+
+        else if (0.49f >= currentHealth && currentHealth >= 0.31f)
+            Debug.Log("H");
+
+        else if (0.30 >= currentHealth && currentHealth >= 0.21f)
+            Debug.Log("L");
+
+        else if (currentHealth >= 0.20f)
+            Debug.Log("LL");
+
+        else
+            Debug.Log("Error");
+
+
+        if (currentHealth >= 0.05f)
+            obj.transform.localScale = new Vector3(radius, radius, 1.0f);
+    }
+}
