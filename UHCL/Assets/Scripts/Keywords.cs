bc7d0d24a872354908567b4c21daef2b47b15ac2
Travis Samford <samfordt@gmail.com>
2018-03-22 14:37:56 -0500

    Added the project back to github
    Our gitIgnore file was in the wrong directory when the project was first uploaded. This caused many files to be tracked that shouldnt have been.

---
+++ UHCL/Assets/Scripts/Keywords.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using System;
+
+public class Keywords : MonoBehaviour,ISpeechHandler {
+
+    ///This script is where voice command functionality is defined 
+    ///The buisness layer will recieve voice commands and resolve them
+
+    //Need Instance of buisnees layer 
+    public bool IsGlobalListener = true;
+
+    public TextMesh testText;
+
+    public GameObject commonData;
+    private CommonData data;
+
+    private void OnEnable()
+    {
+        InputManager.Instance.AddGlobalListener(gameObject);
+    }
+
+    void Awake()
+    {
+        data = CommonData.GetInstance();
+    }
+
+    public void OnSpeechKeywordRecognized(SpeechEventData eventData)
+    {
+        switch (eventData.RecognizedText.ToLower())
+        {
+            case "start edit mode":
+                {
+                    StartEditMode();
+                    break;
+                }
+            case "stop edit mode":
+                {
+                    StopEditMode();
+                    break;
+                }
+            default:
+                break;
+        }
+    }
+
+    private void StopEditMode()
+    {
+        testText.text = "Stop edit mode.";
+        data.EditMode = false;
+    }
+
+    private void StartEditMode()
+    {
+        testText.text = "Start edit mode.";
+        data.EditMode = true;
+    }
+}
