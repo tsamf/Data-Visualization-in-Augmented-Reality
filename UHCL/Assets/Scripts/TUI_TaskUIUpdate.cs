80da8004c30e83662ea67a301b4a8bc593194320
LAPTOP-G5I40IVK\nik <netaji.yadla@gmail.com>
2018-04-04 14:05:57 -0500

    Created the TASKUI Update Script

---
+++ UHCL/Assets/Scripts/TUI_TaskUIUpdate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TUI_TaskUIUpdate : MonoBehaviour {
+    CommonData commonData = CommonData.GetInstance();
+
+    public Text previousTask;
+    public Text currentTask;
+    public Text nextTask;
+    public GameObject imagePanel;
+    public GameObject cautionPanel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //currentTask.text = commonData.getCurrentTask().Text;
+        imagePanel.GetComponent<Image>().sprite = Resources.Load<Sprite>("eva box");
+
+	}
+}
