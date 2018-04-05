b93054b01add6652493593631b4d2fa3454408f1
Travis Samford <samfordt@gmail.com>
2018-04-05 14:20:05 -0500

    Added the Task UI scripts and updates

--- UHCL/Assets/Scripts/DB_TaskTesting.cs
+++ UHCL/Assets/Scripts/DB_TaskTesting.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class DB_TaskTesting : MonoBehaviour {
+
+    CommonData commonData = CommonData.GetInstance();
 
     private Procedure procedure;
 
@@ -11,21 +14,21 @@ public class DB_TaskTesting : MonoBehaviour {
         UITask task1 = new UITask();
         task1.stepNumber = 1;
         task1.text = "On the RIGHT side of the EVA Kit, locate the PANEL ACCESS KEY.";
-        task1.caution = "The keys are on a tension-spring cable.";
-        task1.warning = " ";
+        task1.caution = "";
+        task1.warning = " tension-spring cable.";
         task1.holograms = " ";
         task1.images = " ";
         UITask task2 = new UITask();
-        task2.stepNumber = 2 ;
+        task2.stepNumber = 2; ;
         task2.text = "Use the PANEL ACCESS KEY to unlock the PANEL ACCESS DOOR LOCKS.";
-        task2.caution = "";
+        task2.caution = "The keys are on a tension-spring cable.";
         task2.warning = " ";
         task2.holograms = " ";
         task2.images = " ";
         UITask task3 = new UITask();
         task3.stepNumber = 3 ;
         task3.text = "Carefully return keys to the side of the EVA kit.";
-        task3.caution = "";
+        task3.caution = "Door can accidentally close";
         task3.warning = " ";
         task3.holograms = " ";
         task3.images = " ";
@@ -46,20 +49,28 @@ public class DB_TaskTesting : MonoBehaviour {
 
 
 
-
-
-
         procedure.tasks.Add(task1);
         procedure.tasks.Add(task2);
         procedure.tasks.Add(task3);
         procedure.tasks.Add(task4);
         procedure.tasks.Add(task5);
-	}
+
+        CommonData.GetInstance().previousTask =task1 ;
+        CommonData.GetInstance().currentTask = task2;
+        CommonData.GetInstance().nextTask = task3;
+
+
+ 
+
+
+
+    }
 
     public Procedure GetProcedure()
     {
         return procedure;
     }
+    
 }
 
 public class UITask
--- UHCL/Assets/Scripts/TUI_TaskUIUpdate.cs
+++ UHCL/Assets/Scripts/TUI_TaskUIUpdate.cs
@@ -2,25 +2,107 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class TUI_TaskUIUpdate : MonoBehaviour {
     CommonData commonData = CommonData.GetInstance();
+
 
     public Text previousTask;
     public Text currentTask;
     public Text nextTask;
     public GameObject imagePanel;
+    public Text  cautionText;
+    public GameObject taskPanel;
     public GameObject cautionPanel;
+    public Image cautionImage;
+
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        taskPanel.gameObject.SetActive(false);
+        cautionPanel.gameObject.SetActive(false);
+        imagePanel.gameObject.SetActive(false);
+
+    }
 	
 	// Update is called once per frame
-	void Update () {
-        //currentTask.text = commonData.getCurrentTask().Text;
-      //  imagePanel.GetComponent<Image>().sprite = Resources.Load<Sprite>("eva box");
+	void Update ()
+    {
 
-	}
+        currentTask.text = "TASK ARRIVED";
+
+        if (commonData.startProcedure)
+        {
+            taskPanel.gameObject.SetActive(true);
+            previousTask.text = commonData.GetPreviousTask.text;
+            currentTask.text = commonData.GetCurrentTask.text;
+            nextTask.text = commonData.GetNextTask.text;
+            imagePanel.gameObject.SetActive(true);
+            imagePanel.GetComponent<Image>().sprite = Resources.Load<Sprite>("key");
+            cautionText.text = commonData.GetCurrentTask.caution;
+            if (cautionText.text != null)
+            {
+                cautionPanel.gameObject.SetActive(true);
+                cautionPanel.GetComponent<Image>().color = Color.yellow;
+               //cautionPanel.GetComponent<Image>().
+
+            }
+
+            if (commonData.nextStep)
+            {
+                nextTask.color = Color.white;
+                nextTask.fontSize = 16;
+
+                imagePanel.GetComponent<Image>().sprite = Resources.Load<Sprite>("3");
+
+                cautionText.text = commonData.GetNextTask.caution;
+                cautionPanel.GetComponent<Image>().color = Color.yellow;
+                previousTask.color = new Color32(93, 226, 238, 255);
+                previousTask.fontSize = 14;
+                currentTask.color = new Color32(93, 226, 238, 255);
+                currentTask.fontSize = 14;
+
+
+            }
+
+            if (commonData.previousStep)
+            {
+                previousTask.color = Color.white;
+                previousTask.fontSize = 16;
+         
+                imagePanel.GetComponent<Image>().sprite = Resources.Load<Sprite>("4");
+
+                cautionText.text = commonData.GetPreviousTask.warning;
+                cautionPanel.GetComponent<Image>().color = Color.red;
+                currentTask.color = new Color32(93,226,238,255);
+                currentTask.fontSize = 14;
+                nextTask.color = new Color32(93, 226, 238, 255);
+                nextTask.fontSize = 14;
+
+            }
+         
+        }
+     
+
+
+
+
+
+
+    }
+
 }
+	
+
+
+/*  
+    taskPanel.gameObject.SetActive(true);
+     currentTask.text = commonData.GetCurrentTask.text;
+               nextTask.text = commonData.GetNextTask.text;
+               cautionText.text = commonData.GetCurrentTask.caution;
+           if (cautionText.text != null)
+           {
+               cautionPanel.gameObject.SetActive(true);
+               cautionPanel.GetComponent<Image>(). }
+ */
