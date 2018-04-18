ca1dfc44253250fcc606475a4ac1e046174e06c4
KadamA19 <36745180+KadamA19@users.noreply.github.com>
2018-04-18 15:08:15 -0500

    warning Window

--- UHCL/Assets/Scripts/HUI_WarningWindow.cs
+++ UHCL/Assets/Scripts/HUI_WarningWindow.cs
@@ -6,31 +6,32 @@ using UnityEngine.UI;
 public class HUI_WarningWindow : MonoBehaviour {
 
     public GameObject bl_main;
-    public Text warning;
-    public CommonData commonData;
-    public GameObject Panel;
+    public GameObject warningDetailPanel;
+    public Text prevwarning;
+    public Text currentwarning;
+    public Text nextwarning;
+  
 
 
 	// Use this for initialization
 	void Start () {
 
-        commonData = CommonData.GetInstance();
-		
+        warningDetailPanel.SetActive(true);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        bl_main.GetComponent<BL_Main>().bl_alarming.BLAlarming();
-        bool flagL = bl_main.GetComponent<BL_Main>().bl_alarming.SuitPressureLoStatus;
+    //    bl_main.GetComponent<BL_Main>().bl_alarming.BLAlarming();
+    //    bool flagL = bl_main.GetComponent<BL_Main>().bl_alarming.SuitPressureLoStatus;
 
-        if(flagL == true)
-        {
-            Panel.SetActive(true);
-            warning.color = Color.yellow;
-            warning.text = "Pressure Low";
+    //    if(flagL == true)
+     //   {
+     //       Panel.SetActive(true);
+      //      warning.color = Color.yellow;
+       //     warning.text = "Pressure Low";
 
-        }
+       // }
 		
 	}
 }
